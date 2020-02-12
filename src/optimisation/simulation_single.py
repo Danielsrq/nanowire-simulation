9f29bc31594c07c36e8ba8e6ce26b2a0f3ef5b19
Danielsrq <danielsrq2015@gmail.com>
2020-02-12 14:52:01 +0000

    added cost function

--- src/optimisation/simulation_single.py
+++ src/optimisation/simulation_single.py
@@ -3,7 +3,40 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append('../')
 from nanowire import Nanowire
+import more_itertools as mit
 
+def process_spectrum(E, B_step, cost_f, threshold = 1E-6):
+    E = np.array(E)
+    costs = [] # list of costs for each EigenE
+    for i in range(len(E[0])): # For each EigenE
+        energy_single = E[:,i] 
+        zeroE_indices = [i for i,j in enumerate(energy_single) if np.abs(j) < threshold] # index of zero energy points for a given EigenE
+        clusters = [list(cluster) for cluster in mit.consecutive_groups(zeroE_indices)] # list of lists of consecutive indices
+        cost = [cost_f(B_step * np.array([i[0], len(i)])) for i in clusters]  # array of arrays of (B_crit, B_width) 
+        if len(cost) == 0:
+            cost = [0]
+        cost = max(cost) # only interested in the maximum cost of each EigenE line
+        costs.append(cost)
+        # print (i)
+        # print (energy_single)
+        # print (zeroE_indices)
+        # print (clusters)
+        # print (cost)
+        # print ("\n")
+    final_cost = max(costs)
+    # print (final_cost)
+    return final_cost
+
+
+def weighting(arr):
+    """ test weighting function """
+    if len(arr) != 2: # shld not be called
+        print ("array has len != 2")
+        return 0
+    else:
+        Bcrit, B_width = arr[0], arr[1]
+        return  Bcrit + B_width
+    
 
 def simulation_single(params):
     nanowire = Nanowire(
@@ -20,7 +53,7 @@ def simulation_single(params):
         barrier=params["barrier"],
     )
 
-    spectrum_data = nanowire.spectrum(bValues=np.linspace(0, params["b_max"], 81))
+    spectrum_data = nanowire.spectrum(bValues=np.linspace(0, params["b_max"], 21))
     fig = plt.figure()
     plt.rcParams["figure.figsize"] = (7, 5)
     ax = fig.gca()
@@ -29,6 +62,7 @@ def simulation_single(params):
     ax.set_ylabel("Energies [t]")
     print(spectrum_data["CritB"])
     plt.show()
+    return spectrum_data
 
 
 params = dict()
@@ -45,4 +79,10 @@ params['delta'] = 4.5E-05
 params['barrier'] = 0.1
 params['b_max'] = 1
 
-simulation_single(params)
+#%%
+# testing cost function
+spectrum_data = simulation_single(params)
+B, E = spectrum_data["B"], spectrum_data["E"]
+B_step = B[1] - B[0]
+
+cost = process_spectrum(E, B_step, weighting)
