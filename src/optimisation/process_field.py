4e8e0522a543d2abba298e2b407ecdbf3cc6d924
ricktjwong <rickwtj@gmail.com>
2020-03-04 17:01:22 +0000

    Interpolation return vector field with dimensions (grid_x, grid_y, 2)

--- src/optimisation/process_field.py
+++ src/optimisation/process_field.py
@@ -27,28 +27,29 @@ def interpolate_field(file_path: str, nx: int, ny: int):
     zslice = int(z / 2)
     data = np.array(np.loadtxt(file_path))
     data_field = data.reshape(x, y, z, 3, order="F")
-    u, v = data_field[:, :, :, 0][:, :, zslice], \
-        data_field[:, :, :, 1][:, :, zslice]
+    u, v = data_field[:, :, :, 0], data_field[:, :, :, 1]
+    u_sliced, v_sliced = u[:, :, zslice], v[:, :, zslice]
     x_grid, y_grid = np.mgrid[0:x, 0:y]
-    plt.imshow(u.T, extent=(0, x, 0, y), origin='lower')
+    plt.imshow(u_sliced.T, extent=(0, x, 0, y), origin='lower')
     plt.show()
     x_points = [i for i in range(x)]
     y_points = [i for i in range(y)]
     points = np.transpose(np.vstack([x_grid.ravel(), y_grid.ravel()]))
     x_grid_new, y_grid_new = np.mgrid[0:x-1:nx, 0:y-1:ny]
     grid_u = griddata(points,
-                      u.flatten(),
+                      u_sliced.flatten(),
                       (x_grid_new, y_grid_new),
                       method='cubic')
     grid_v = griddata(points,
-                      v.flatten(),
+                      v_sliced.flatten(),
                       (x_grid_new, y_grid_new),
                       method='cubic')
     plt.imshow(grid_u.T, extent=(0, x, 0, y), origin='lower')
     plt.show()
     plt.imshow(grid_v.T, extent=(0, x, 0, y), origin='lower')
     plt.show()
-    return grid_u, grid_v
+    vector_field = np.stack((grid_u, grid_v), axis=-1)
+    return vector_field
 
 
-interpolate_field('./data/strayfield_halbach_100_60.ovf', nx=1000j, ny=1000j)
+interpolate_field('./data/strayfield_halbach_100_60.ovf', nx=(284*5j), ny=(310*5j))
